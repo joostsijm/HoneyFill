97da8a9cd4a516b79dd73587fd612cb0fe312a71
joostsijm <joostsijm@gmail.com>
2016-09-12 21:46:41 +0200

    first try at allowing accounts to thread

--- generate.py
+++ generate.py
@@ -25,9 +25,9 @@ parser.add_argument(
         help='''Path to the file that stores the coords in CSV format:
             <long>,<lat>''')
 parser.add_argument(
-        '-n','--accountnumbers',
-        required=True,
+        '-t','--threads',
         type=int,
+        default="1",
         help='''Number accounts per leaps.''')
 parser.add_argument(
         '-o','--output',
@@ -63,6 +63,18 @@ if os.path.isfile(accpath):
         account_fh = open(args.accounts)
         account_fields = [line.split(",") for line in account_fh]
         accountform = [auth_template.format(line[0].strip(), line[1].strip(), line[2].strip()) for line in account_fields]
+        count = 0
+        accountformthread = []
+        while count < len(accountform):
+            if (len(accountform) - count < 3):
+                break
+            threadedacc = []
+            counttwo = 1
+            while counttwo <= args.threads:
+                counttwo = counttwo + 1
+                threadedacc.append(accountform[count])
+                count = count + 1
+            accountformthread.append(threadedacc)
 
 else:
     print("Account file doesn't exist: {}".format(accpath))
@@ -88,7 +100,7 @@ os.chmod(args.output, 0o755)
 output_fh.write(preamble + "\n")
 output_fh.write(server_template.format(coordform[0]))
 
-location_and_auth = [(i, j) for i, j in itertools.izip(coordform, accountform)]
+location_and_auth = [(i, j) for i, j in itertools.izip(coordform, accountformthread)]
 
 for i, (coords, accounts) in enumerate(location_and_auth):
     output_fh.write(worker_template.format(coords=coordform[i], steps=args.steps, auth=accounts, argsu=args.argument))
